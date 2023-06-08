3c861926326cdb326f76f6c1c077c6d443aefa5d
Abhay Manmode <abhay.manmode@claimgenius.com>
2023-06-08 11:27:52 +0530

    Setup frontend

---
+++ FAQ-Module/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+import List from './components/List.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+  <div class=" ">
+    <div class="bg-gray-500">
+      <div class="container mx-auto">
+        <Navbar></Navbar>
+      </div>
+    </div>
+    <div class="container mx-auto min-h-[700px]">
+      <list></list>
+    </div>
+
+    <div class="bg-gray-500">
+      <div class="container mx-auto m-12">
+        <Footer></Footer>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ FAQ-Module/src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+    <p class="text-xl float-left font-mono  hover:underline decoration-solid mt-5">Frequently Asked Quetions?</p>
+    <div class="text-right mt-3">
+      <button class="btn btn-primary">Create FAQ</button>
+  </div>
+  <div class="overflow-x-auto mt-5">
+    <table class="table table-zebra w-full">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Question</th>
+          <th>Answer</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr class="bg-base-200">
+          <th>1</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+          <td>Purple</td>
+
+        </tr>
+        <!-- row 2 -->
+        <tr>
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+          <td>Purple</td>
+
+        </tr>
+        <!-- row 3 -->
+        <tr>
+          <th>3</th>
+          <td>Brice Swyre</td>
+          <td>Tax Accountant</td>
+          <td>Red</td>
+          <td>Purple</td>
+
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
